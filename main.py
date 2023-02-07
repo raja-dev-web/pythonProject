ae370c56c2ea29d11a024cfedc148b5a6af37983
Arokiyaraja <aland.arokiyaraja@hotmail.com>
2023-02-07 14:39:48 +0100

    Inherited the attributes of parent class from child class

--- main.py
+++ main.py
@@ -46,4 +46,18 @@ class Item:
         return f"Item('{self.name}', {self.price_unit}, {self.quantity})"
 
 
-print(Item.check_is_integer(7))
+class Mobile(Item):
+    all = []
+
+    def __init__(self, name: str, price_unit: float, quantity=0, broken_phone=0):
+        super().__init__(
+            name, price_unit, quantity
+        )
+        assert broken_phone >= 0, f"Broken phone {broken_phone} must be greater than or equal to zero!"
+        self.broken_phone = broken_phone
+        # print("Item Created")
+        Mobile.all.append(self)
+
+
+mobile1 = Mobile("samsung", 8000, 3, 1)
+print(mobile1.calculate_total_price())
